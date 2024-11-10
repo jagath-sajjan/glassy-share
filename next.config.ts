c5ecc6417e7bbe908ae98e48dcb6f97c7d8f14dc
jagath-sajjan <jagathsajjan097@gmail.com>
2024-11-10 13:08:42 +0530

    done-7

--- next.config.ts
+++ next.config.ts
@@ -1,36 +1,7 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ['redis'],
-  webpack: (config: import('webpack').Configuration, { dev, isServer }: { dev: boolean, isServer: boolean }) => {
-    config.cache = true; // Enable Webpack cache
-
-    if (!dev && !isServer) {
-      config.optimization = {
-        ...config.optimization,
-        splitChunks: {
-          chunks: 'all',
-          minSize: 20000,
-          maxSize: 244000,
-          minChunks: 1,
-          maxAsyncRequests: 30,
-          maxInitialRequests: 30,
-          cacheGroups: {
-            defaultVendors: {
-              test: /[\\/]node_modules[\\/]/,
-              priority: -10,
-              reuseExistingChunk: true
-            },
-            default: {
-              minChunks: 2,
-              priority: -20,
-              reuseExistingChunk: true
-            }
-          }
-        }
-      };
-    }
-    return config;
-  }
 };
 
 module.exports = nextConfig;
